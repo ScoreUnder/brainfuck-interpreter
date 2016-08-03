38d90e6549f3696e591aa5e989d22ec51ea8e28f
Score_Under <seejay.11@gmail.com>
2016-08-03 13:46:14 +0100

    Implement negative tape offsets differently
    Just memmove the current values on the tape forward, and correct the
    position to compensate.
    
    As usual, "inline" and "restrict" were added because they were found to
    significantly improve execution speed.

--- interpreter.c
+++ interpreter.c
@@ -14,68 +14,73 @@ typedef struct {
 } tape_struct;
 #else
 typedef struct {
-	size_t size;
-	size_t back_size;
-	ssize_t pos;
+	size_t back_size;  // Amount of tape corresponding to negative offsets (used for smarter reallocation purposes)
+	size_t front_size; // Amount of tape corresponding to positive offsets (used for smarter reallocation purposes)
+	size_t pos;
 	cell_int *restrict cells;
-	cell_int *restrict back_cells;
+#if !defined(NDEBUG) && !defined(FIXED_TAPE_SIZE)
+	size_t bound_upper, bound_lower;
+#endif
 } tape_struct;
 #endif
 
 #ifndef FIXED_TAPE_SIZE
-static void tape_ensure_space(tape_struct *tape, ssize_t pos) {
-	if (pos >= 0) {
-		if (tape->size <= (size_t) pos) {
-			size_t old_size = tape->size;
-			while (tape->size <= (size_t) pos) {
-				tape->size *= 2;
-			}
-			tape->cells = realloc(tape->cells, tape->size * sizeof *tape->cells);
-			memset(tape->cells + old_size, 0, tape->size - old_size);
+inline static void tape_ensure_space(tape_struct *restrict tape, ssize_t pos) {
+	if (pos < 0) {
+		size_t old_back_size = tape->back_size;
+		while (pos < 0) {
+			pos += tape->back_size;
+			tape->back_size *= 2;
 		}
-	} else {
-		pos = -pos;
-		if (tape->back_size < (size_t)pos) {
-			size_t old_size = tape->back_size;
-			while (tape->back_size < (size_t)pos) {
-				tape->back_size *= 2;
-			}
-			tape->back_cells = realloc(tape->back_cells, tape->back_size * sizeof *tape->back_cells);
-			memset(tape->back_cells + old_size, 0, tape->back_size - old_size);
+		size_t extra_size = tape->back_size - old_back_size;
+		size_t total_size = tape->back_size + tape->front_size;
+		tape->cells = realloc(tape->cells, total_size * sizeof *tape->cells);
+		memmove(tape->cells + extra_size, tape->cells, (tape->front_size + old_back_size) * sizeof *tape->cells);
+		memset(tape->cells, 0, extra_size * sizeof *tape->cells);
+
+		tape->pos += extra_size;
+#if !defined(NDEBUG) && !defined(FIXED_TAPE_SIZE)
+		tape->bound_upper += extra_size;
+		// tape->bound_lower += extra_size; // XXX assuming checking left means bound_lower will be overwritten
+#endif
+	} else if (pos >= (ssize_t)(tape->back_size + tape->front_size)) {
+		size_t old_front_size = tape->front_size;
+
+		size_t total_size = tape->front_size + tape->back_size;
+		while ((size_t)pos >= total_size) {
+			total_size += tape->front_size;
+			tape->front_size *= 2;
 		}
+
+		size_t extra_size = tape->front_size - old_front_size;
+		tape->cells = realloc(tape->cells, total_size * sizeof *tape->cells);
+		memset(tape->cells + old_front_size + tape->back_size, 0, extra_size * sizeof *tape->cells);
 	}
 }
 #endif
 
 void execute_bf(char *restrict what) {
-#ifdef FIXED_TAPE_SIZE
-#define CELL tape.cells[tape.pos]
-#else
-#define CELL *(tape.pos >= 0 ? &tape.cells[tape.pos] : &tape.back_cells[-1 - tape.pos])
-#endif
-
-#if !defined(NDEBUG) && !defined(FIXED_TAPE_SIZE)
-	ssize_t bound_upper = 0, bound_lower = 0;
-#endif
-
 	tape_struct tape = {
-		.pos = 0,
+		.pos = 16,
 #ifndef FIXED_TAPE_SIZE
-		.size = 16,
 		.back_size = 16,
+		.front_size = 16,
+#ifndef NDEBUG
+		.bound_upper = 16,
+		.bound_lower = 16,
+#endif
 #endif
 	};
 #ifdef FIXED_TAPE_SIZE
 	tape.cells = calloc(sizeof *tape.cells, (size_t)256 << sizeof(FIXED_TAPE_SIZE));
 #else
-	tape.cells = calloc(sizeof *tape.cells, tape.size);
-	tape.back_cells = calloc(sizeof *tape.back_cells, tape.back_size);
+	tape.cells = calloc(sizeof *tape.cells, tape.front_size + tape.back_size);
 #endif
 
 	while (true) {
 #if !defined(NDEBUG) && !defined(FIXED_TAPE_SIZE)
-		if (tape.pos < bound_lower || tape.pos > bound_upper)
-			errx(2, "Bounds check failure. Pos: %zd, expected <%zd - %zd>\n", tape.pos, bound_lower, bound_upper);
+		if (tape.pos < tape.bound_lower || tape.pos > tape.bound_upper)
+			errx(2, "Bounds check failure. Pos: %zu, expected <%zu - %zu>\n", tape.pos, tape.bound_lower, tape.bound_upper);
 #endif
 		switch (*what++) {
 			case BF_OP_ALTER: {
@@ -85,7 +90,7 @@ void execute_bf(char *restrict what) {
 				what += sizeof(cell_int);
 
 				tape.pos += offset;
-				CELL += amount;
+				tape.cells[tape.pos] += amount;
 				break;
 			}
 
@@ -94,15 +99,16 @@ void execute_bf(char *restrict what) {
 				ssize_t offset = *(ssize_t*)what;
 				what += sizeof(ssize_t);
 
+				tape_ensure_space(&tape, tape.pos + offset);
+
 #ifndef NDEBUG
 				if (offset < 0) {
-					bound_lower = tape.pos + offset;
+					tape.bound_lower = tape.pos + offset;
 				} else {
-					bound_upper = tape.pos + offset;
+					tape.bound_upper = tape.pos + offset;
 				}
 #endif
 
-				tape_ensure_space(&tape, tape.pos + offset);
 				break;
 			}
 #endif
@@ -119,7 +125,7 @@ void execute_bf(char *restrict what) {
 				cell_int amount = *(cell_int*)what;
 				what += sizeof(cell_int);
 
-				CELL += amount;
+				tape.cells[tape.pos] += amount;
 				break;
 			}
 
@@ -132,13 +138,13 @@ void execute_bf(char *restrict what) {
 					cell_int amount = *(cell_int*)what;
 					what += sizeof(cell_int);
 
-					cell_int orig = CELL;
+					cell_int orig = tape.cells[tape.pos];
 					if (orig == 0) {
 						what += (sizeof(ssize_t) + sizeof(cell_int)) * repeat;
 						break;
 					}
 					tape.pos += offset;
-					CELL += orig * amount;
+					tape.cells[tape.pos] += orig * amount;
 					tape.pos -= offset;
 				} while (repeat--);
 
@@ -149,19 +155,19 @@ void execute_bf(char *restrict what) {
 				cell_int amount = *(cell_int*)what;
 				what += sizeof(cell_int);
 
-				CELL = amount;
+				tape.cells[tape.pos] = amount;
 				break;
 			}
 
 			case BF_OP_IN: {
 				int input = getchar();
 				if (input == EOF && sizeof(cell_int) == 1) input = 0;
-				CELL = input;
+				tape.cells[tape.pos] = input;
 				break;
 			}
 
 			case BF_OP_OUT:
-				putchar(CELL);
+				putchar(tape.cells[tape.pos]);
 				break;
 
 
@@ -169,7 +175,7 @@ void execute_bf(char *restrict what) {
 				ssize_t offset = *(ssize_t*)what;
 				what += sizeof(ssize_t);
 
-				while (CELL != 0) {
+				while (tape.cells[tape.pos] != 0) {
 					tape.pos += offset;
 #ifndef FIXED_TAPE_SIZE
 					tape_ensure_space(&tape, tape.pos);
@@ -178,9 +184,9 @@ void execute_bf(char *restrict what) {
 
 #if !defined(NDEBUG) && !defined(FIXED_TAPE_SIZE)
 				if (offset < 0)
-					bound_lower = tape.pos;
+					tape.bound_lower = tape.pos;
 				else
-					bound_upper = tape.pos;
+					tape.bound_upper = tape.pos;
 #endif
 				break;
 			}
@@ -188,7 +194,7 @@ void execute_bf(char *restrict what) {
 			case BF_OP_JUMPIFZERO: {
 				ssize_t offset = *(ssize_t*)what;
 				what += sizeof(ssize_t);
-				if (CELL == 0) {
+				if (tape.cells[tape.pos] == 0) {
 					what += offset;
 				}
 				break;
@@ -197,7 +203,7 @@ void execute_bf(char *restrict what) {
 			case BF_OP_JUMPIFNONZERO: {
 				ssize_t offset = *(ssize_t*)what;
 				what += sizeof(ssize_t);
-				if (CELL != 0) {
+				if (tape.cells[tape.pos] != 0) {
 					what += offset;
 				}
 				break;
@@ -210,5 +216,4 @@ void execute_bf(char *restrict what) {
 				errx(1, "Invalid internal state");
 		}
 	}
-#undef CELL
 }
