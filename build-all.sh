b2d8b37a39d8c4953eaadeadced3b78d32404a50
Score_Under <seejay.11@gmail.com>
2016-08-08 01:10:08 +0100

    Make build-all.sh build every configuration; add clean option
    build-all.sh runs the gamut of all possible bits/debug configurations,
    for brainfuck and now for brainfuck2c.
    It does not, however, include FIXED_TAPE_SIZE builds. That would just
    get ridiculous. Quadruple the already excessive number of executables!
    
    Running "./build-all.sh clean" should clean up all built artifacts.

--- build-all.sh
+++ build-all.sh
@@ -1,15 +1,55 @@
 #!/bin/sh
-build_opt() { make -B CPPFLAGS="$2 -DNDEBUG" && mv brainfuck "$1"; }
-build_debug() { make -B CFLAGS='-Og -ggdb -std=c11 -Wall -Wextra -pedantic' CPPFLAGS="$2" && mv brainfuck "$1"; }
+build_opt() { make -B CPPFLAGS="$1 -DNDEBUG"; }
+build_debug() { make -B CFLAGS='-Og -ggdb -std=c11 -Wall -Wextra -pedantic' CPPFLAGS="$1"; }
 
 build() {
-    printf '> Building %s\n' "$1"
-    build_opt "$1" "$2"
-    printf '> Building %s-debug\n' "$1"
-    build_debug "$1-debug" "$2"
+    name=$1
+    extra_cppflags=$2
+
+    printf '> Building %s\n' "brainfuck-$name"
+    build_opt "$extra_cppflags"
+    for f in 'brainfuck' 'brainfuck2c'; do
+        mv -- "$f" "$f-$name"
+    done
+
+    name=$1-debug
+    printf '> Building %s\n' "brainfuck-$name"
+    build_debug "$extra_cppflags"
+    for f in 'brainfuck' 'brainfuck2c'; do
+        mv -- "$f" "$f-$name"
+    done
 }
 
-build brainfuck-8
-build brainfuck-16 -DCELL_INT=int16_t
-build brainfuck-32 -DCELL_INT=int32_t
-build brainfuck-64 -DCELL_INT=int64_t
+echo_and_run() {
+    printf '%s ' "$@"
+    printf \\n
+    "$@"
+}
+
+clean() {
+    set --
+    for bits in 8 16 32 64; do
+        for type in '' '-debug'; do
+            for executable in brainfuck- brainfuck2c-; do
+                set -- "$@" "$executable$bits$type"
+            done
+        done
+    done
+    echo_and_run rm -f -- "$@"
+}
+
+case $1 in
+    (clean)
+        make clean
+        clean
+        ;;
+    (''|all)
+        for bits in 8 16 32 64; do
+            build "$bits" -DCELL_INT="int${bits}_t"
+        done
+        ;;
+    (*)
+        printf 'Unknown argument %s\n' "$1" >&2
+        exit 1
+        ;;
+esac
